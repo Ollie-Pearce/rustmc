1ee4a046de868f37f98e441c1ba09711d01bfe8a
Ollie <75216562+ZJAC281@users.noreply.github.com>
2025-02-01 18:15:38 +0000

    Tweaked verify.sh

--- verify.sh
+++ verify.sh
@@ -1,17 +1,39 @@
-TARGET_RUST_PROJECT=$1
+if [ "$#" -lt 1 ]; then
+	echo "Target Cargo Project not supplied. Exiting"
+	exit 1
+fi
 
+TARGET_RUST_PROJECT=$1
+MIXED_LANGUAGE=false
 DEPDIR=$(pwd)
 
-cd $DEPDIR/genmc
+shift
 
-make
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -ffi)
+      MIXED_LANGUAGE=true
+      shift  # consume this argument
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exit 1
+      shift
+      ;;
+  esac
+done
 
-cd $1
+cd $TARGET_RUST_PROJECT
 
 cargo clean
- 
+
 export RUSTFLAGS=" -C overflow-checks=off -C panic=abort --emit=llvm-bc -C opt-level=0 -C debuginfo=2 -C llvm-args=--inline-threshold=9000 -C llvm-args=--bpf-expand-memcpy-in-order -C no-prepopulate-passes -C passes=ipsccp -C passes=globalopt -C passes=reassociate -C passes=argpromotion -C passes=typepromotion -C passes=lower-constant-intrinsics  -C passes=memcpyopt -C passes=dse"
 rustup run RustMC cargo run --target x86_64-unknown-linux-gnu 
+
+if [ "$MIXED_LANGUAGE" = "true" ]; then
+	clang -O0 -emit-llvm -c *.c
+	mv *.bc $(pwd)/target/x86_64-unknown-linux-gnu/debug/deps
+fi
 
 find $(pwd)/target/x86_64-unknown-linux-gnu/debug/deps -name "*.bc" > $DEPDIR/bitcode.txt
 
