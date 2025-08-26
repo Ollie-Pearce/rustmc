e8b77c290d7e79f5c7642911c5a2fcfc027a3ef6
Ollie <zjac281@live.rhul.ac.uk>
2025-08-26 01:04:35 +0100

    Added pfds verify script

---
+++ pfds_verify.sh
@@ -0,0 +1,96 @@
+TARGET_RUST_PROJECT=verify_test_benchmarks/pfds
+MIXED_LANGUAGE=false
+DEPDIR=$(pwd)
+
+rm -rf verify_test_benchmarks/pfds
+rm -rf test_results/*.txt
+cd verify_test_benchmarks
+git clone https://github.com/NeoCogi/pfds.git
+cd ..
+
+make
+
+cd $TARGET_RUST_PROJECT
+
+find . -name "*.rs" | while read -r file; do
+  awk '
+    {
+      if ($0 ~ /^[[:space:]]*#\[test\]/ && prev !~ /^[[:space:]]*#\[no_mangle\]/) {
+        print "#[no_mangle]"
+      }
+      print
+      prev = $0
+    }
+  ' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+done
+
+find . -name "*.rs" | while read -r file; do
+  filename=$(basename "$file" .rs)
+  awk -v prefix="${filename}_" '
+    /^[[:space:]]*#\[test\]/ { in_test = 1; print; next }
+    in_test && /^[[:space:]]*fn[[:space:]]+([a-zA-Z0-9_]+)/ {
+      if (match($0, /fn[[:space:]]+([a-zA-Z0-9_]+)/, m)) {
+        old_name = m[1]
+        sub(old_name, prefix old_name)
+      }
+      in_test = 0
+    }
+    { print }
+  ' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+done
+
+echo "Collecting #[test] function names..."
+TEST_FUNCS_FILE="$DEPDIR/test_functions.txt"
+> "$TEST_FUNCS_FILE"
+
+find . -name "*.rs" | while read -r file; do
+  awk '
+    BEGIN { in_test = 0 }
+    /^[[:space:]]*#\[test\]/ { in_test = 1; next }
+    in_test && /^[[:space:]]*fn[[:space:]]+[a-zA-Z0-9_]+/ {
+      if (match($0, /fn[[:space:]]+([a-zA-Z0-9_]+)/, m)) {
+        print m[1]
+      }
+      in_test = 0
+    }
+  ' "$file"
+done >> "$TEST_FUNCS_FILE"
+
+
+
+cargo clean
+
+RUSTFLAGS="--emit=llvm-bc -Zpanic_abort_tests -C overflow-checks=off -C prefer-dynamic=no -C codegen-units=1 -C lto=no -C opt-level=0 -C debuginfo=2 -C llvm-args=--inline-threshold=9000 -C llvm-args=--bpf-expand-memcpy-in-order -C no-prepopulate-passes -C codegen-units=1 -C passes=ipsccp -C passes=globalopt -C passes=reassociate -C passes=argpromotion -C passes=typepromotion -C passes=lower-constant-intrinsics  -C passes=memcpyopt -Z mir-opt-level=0 " rustup run RustMC cargo test --target-dir target-ir --no-run
+
+echo "pwd is $(pwd)"
+echo "DEPDIR is $DEPDIR"
+
+find $(pwd)/target-ir/debug/deps -name "*.bc" > $DEPDIR/bitcode.txt
+
+cd $DEPDIR
+
+rm combined.ll
+
+llvm-link --internalize -S --override=$DEPDIR/override/my_pthread.ll -o combined.ll @bitcode.txt
+
+
+
+while read -r test_func; do
+  echo "Verifying test function: $test_func"
+  timeout 20s ./genmc --mixer \
+          --transform-output=myout.ll \
+          --print-exec-graphs \
+          --disable-function-inliner \
+          --program-entry-function="$test_func" \
+          --disable-estimation \
+          --print-error-trace \
+          --disable-stop-on-system-error \
+          combined.ll > "test_results/${test_func}_verification.txt" 2>&1
+
+  if [ $? -eq 124 ]; then
+    echo "TIMEOUT" >> "test_results/${test_func}_verification.txt"
+  fi
+done < "$TEST_FUNCS_FILE"
+
+
+#./genmc --transform-output=myout.ll --print-exec-graphs --disable-function-inliner --program-entry-function="hashmap_insert_verification.txt" --disable-estimation --print-error-trace --disable-stop-on-system-error ../combined.ll > "../test_results/hashmap_insert_verification.txt"
