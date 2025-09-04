9e4483dfa4c1b0dfbd555ef24e44a7a650fa0104
Ollie <zjac281@live.rhul.ac.uk>
2025-09-04 09:03:00 +0100

    Added tests package for state

---
+++ verify_test_benchmarks/state_tests/target-ir/debug/deps/state_tests-a9006183836f6917.d
@@ -0,0 +1,7 @@
+/home/ollie/Desktop/mixer/genmc-tool/verify_test_benchmarks/state_tests/target-ir/debug/deps/state_tests-a9006183836f6917.bc: src/main.rs
+
+/home/ollie/Desktop/mixer/genmc-tool/verify_test_benchmarks/state_tests/target-ir/debug/deps/state_tests-a9006183836f6917: src/main.rs
+
+/home/ollie/Desktop/mixer/genmc-tool/verify_test_benchmarks/state_tests/target-ir/debug/deps/state_tests-a9006183836f6917.d: src/main.rs
+
+src/main.rs:
