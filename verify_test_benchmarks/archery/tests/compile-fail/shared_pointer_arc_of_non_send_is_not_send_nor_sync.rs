c5dac9ce6aabfd4906596038936af24dfa1e4788
Ollie <zjac281@live.rhul.ac.uk>
2025-09-12 05:18:39 +0100

    added archery benchmark

---
+++ verify_test_benchmarks/archery/tests/compile-fail/shared_pointer_arc_of_non_send_is_not_send_nor_sync.rs
@@ -0,0 +1,11 @@
+use archery::*;
+use static_assertions::assert_impl_all;
+use std::sync::MutexGuard;
+
+assert_impl_all!(SharedPointer<MutexGuard<'static, ()>, ArcK>: Send);
+//~^ ERROR cannot be sent between threads safely
+
+assert_impl_all!(SharedPointer<MutexGuard<'static, ()>, ArcK>: Sync);
+//~^ ERROR cannot be sent between threads safely
+
+fn main() {}
