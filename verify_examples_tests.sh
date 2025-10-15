222a24b0803ea88eb6c74915e754410e57fdf3ba
Ollie <zjac281@live.rhul.ac.uk>
2025-10-15 14:30:21 +0100

    Added override

--- verify_examples_tests.sh
+++ verify_examples_tests.sh
@@ -172,6 +172,8 @@ find "$(pwd)/target-ir/debug/deps" -type f -name "examples-*.ll" >> "$DEPDIR/bit
 cd $DEPDIR
 
 /usr/bin/llvm-link-18 --internalize -S --override=$DEPDIR/override/my_pthread.ll -o combined_old.ll @bitcode.txt
+/usr/bin/llvm-link-18 --internalize -S --override=$TARGET_RUST_PROJECT/target-ir/debug/deps/sdd-8263b49b5a5542e7.ll -o combined_old.ll
+
 /usr/bin/opt-18 -S -mtriple=x86_64-unknown-linux-gnu -expand-reductions combined_old.ll -o combined.ll
 
 echo " "
